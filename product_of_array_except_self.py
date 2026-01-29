898a94e111713820d6a2a1c7b4e57da339589ef4
rachit74 <rachithooda09@gmail.com>
2026-01-29 11:00:25 +0530

    product_of_array_except_self solve

---
+++ product_of_array_except_self.py
@@ -0,0 +1,27 @@
+
+
+def productExceptSelf(nums: list) -> list :
+
+    n = len(nums)
+    output = [1]*n
+    
+    # left product
+    left = 1
+    for i in range(n):
+        output[i] = left
+        left = left * nums[i]
+
+    # right product
+    right = 1
+    # pythons range function can have (start, stop, step_size)
+    for i in range(n-1, -1, -1):
+        output[i] = output[i] * right
+        right = right * nums[i]
+
+    print(nums)
+    print(output)
+
+
+nums = [1,2,4,6]
+
+productExceptSelf(nums)
